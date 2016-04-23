1e36deb587ef54b289d2def792872be4e22285af
jbraga@ThinkPad <juanbragabrum@gmail.com>
2016-04-22 21:49:19 -0300

    minor changes

--- pitch_extraction.py
+++ pitch_extraction.py
@@ -6,12 +6,12 @@ import matplotlib.pyplot as plt
 import copy
 
 
-fragment = '../traditional_dataset/density/fragments/density_first_fragment_zoon'
+#fragment = '../traditional_dataset/density/fragments/density_first_fragment_zoon'
 
 #fragment = '../traditional_dataset/syrinx/fragments/syrinx_first_fragment_douglas'
 #fragment = '../traditional_dataset/syrinx/fragments/syrinx_second_fragment_dwyer'
 #fragment = '../traditional_dataset/syrinx/fragments/syrinx_third_fragment_rhodes'
-#fragment = '../traditional_dataset/syrinx/fragments/syrinx_fourth_fragment_bernold'
+fragment = '../traditional_dataset/syrinx/fragments/syrinx_fourth_fragment_bernold'
 #fragment = '../traditional_dataset/syrinx/fragments/syrinx_fifth_fragment_bourdin'
 
 #fragment = '../traditional_dataset/allemande/fragments/allemande_second_fragment_gerard'
